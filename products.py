075e2dcd6666796b6c76cfa87ccf625387eead00
heavsmac <hefbolzen@gmail.com>
2025-10-20 20:09:23 +0200

    made setter and getter method for quantity + activate ability with activate deactivate plus status check of active

--- products.py
+++ products.py
@@ -7,12 +7,31 @@ class Product:
 
         if not name:
             raise Exception("Product name cant be empty")
-        if price <=0:
+        if price < 0:
             raise Exception("Price can not be negative")
-        if quantity <=0:
+        if quantity < 0:
             raise Exception("If quantity is below zero , we can not sell or buy any")
 
         self.name = name
         self.price = price
         self.quantity = quantity
         self.active = True
+
+    def get_quantity(self):
+        """ getter method for quantity"""
+        return self.quantity
+
+    def set_quantity(self,quantity):
+        self.quantity = quantity
+        if self.quantity == 0:
+            self.deactivate()
+
+    def activate(self):
+        return self.active == True
+
+    def deactivate(self):
+        return self.active == False
+
+    def is_active(self):
+        return self.active
+
