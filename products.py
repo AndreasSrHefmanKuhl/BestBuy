eab5064aa521163dbf247c6b1e48f9a9148135e9
heavsmac <hefbolzen@gmail.com>
2025-10-21 09:28:15 +0200

    build show method

--- products.py
+++ products.py
@@ -1,9 +1,7 @@
-from csv import excel
-
 
 class Product:
 
-    def __init__(self,name=str,price=float,quantity=int):
+    def __init__(self ,name=str ,price=float ,quantity=int):
 
         if not name:
             raise Exception("Product name cant be empty")
@@ -21,7 +19,7 @@ class Product:
         """ getter method for quantity"""
         return self.quantity
 
-    def set_quantity(self,quantity):
+    def set_quantity(self ,quantity):
         self.quantity = quantity
         if self.quantity == 0:
             self.deactivate()
@@ -38,3 +36,27 @@ class Product:
     def show(self):
         print(f"{self.name}, Price:{self.price}, Quantity:{self.quantity}")
 
+    def buy(self ,quantity):
+
+        if not self.is_active():
+
+            raise Exception(f"Cannot buy {self.name}: Product is inactive.")
+
+        if quantity > self.quantity:
+
+            raise Exception(
+                f"Cannot buy {quantity} units of {self.name}. "
+                f"Only {self.quantity} available."
+            )
+
+        if quantity <= 0:
+
+            raise Exception("Quantity to buy must be a positive integer.")
+
+        total_price = (self.price * quantity)
+
+        updated_quantity = (self.quantity - quantity)
+
+        self.set_quantity(updated_quantity)
+
+        return total_price
