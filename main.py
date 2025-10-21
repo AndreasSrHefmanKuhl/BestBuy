f4fa15560dacddb7595537db5c619c46c71d6c02
heavsmac <hefbolzen@gmail.com>
2025-10-21 19:43:05 +0200

    fixed store class too

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ import ui_functions
 
 
 def main():
-    # setup initial stock of inventory
+
     product_list = [products.Product("MacBook Air M2", price=1450, quantity=100),
                     products.Product("Bose QuietComfort Earbuds", price=250, quantity=500),
                     products.Product("Google Pixel 7", price=500, quantity=250)]
 
     best_buy = store.Store(product_list)
 
-    # Start the application loop
+
     ui_functions.start(best_buy)
 
 
